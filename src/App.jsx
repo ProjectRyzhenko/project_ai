78311bfe7c155888a3c42e15d231e2cca87273e5
School <111399819+ProjectRyzhenko@users.noreply.github.com>
2022-11-01 04:29:11 +0200

    222

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from "./style";
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import { Billing, Business, Business2, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
@@ -21,6 +21,7 @@ const App = () => (
         <Business />
         <Billing />
         <CardDeal />
+        <Business2 />
         <Testimonials />
         <Clients />
         <CTA />
